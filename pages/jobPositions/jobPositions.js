f8b4d72b392a4d823643abf59ef9c274dd2f94d6
Pongsatorn001 <Pongsatorn698@gmail.com>
2018-11-26 12:12:22 +0700

    เพิ่ม Bastcame

--- pages/jobPositions/jobPositions.js
+++ pages/jobPositions/jobPositions.js
@@ -3,8 +3,9 @@ import { withLayout } from '../../hoc'
 import { compose, withProps , withState , withHandlers} from 'recompose'
 import styled from 'styled-components'
 import Link from 'next/link'
-import { Button , Icon , Table } from 'semantic-ui-react'
+import { Button , Icon , Table , Modal , Header } from 'semantic-ui-react'
 import { TextHeaderTable } from '../../components/TextHeader'
+import theme from '../../theme/default';
 
 const enhance = compose(
     withProps({
@@ -12,12 +13,18 @@ const enhance = compose(
     }),
     withLayout,
     withState('list','setList',[{id: '001', nameJobPositions: 'Fontend Developer', ReceivingNumber: '2'},{id: '002', nameJobPositions: 'Fontend Developer', ReceivingNumber: '5'}]),
-    withState('closeModel','setCloseModel',false),
+    withState('open' , 'setOpen' , false),
     withHandlers({
-        handleOnClickCancel: props => l => event => {
-            props.setCloseModel(false)
+        handleModalOpen: props => () => event => {
+            props.setOpen(true)
         },
-    })
+        handleModalClose: props => () => event => {
+            props.setOpen(false)
+        },
+        handleDeleteJobPositions: props => () => event => {
+            props.setOpen(false)
+        }
+    }),
 )
 
 const TablePosition = styled(Table)`
@@ -25,17 +32,21 @@ const TablePosition = styled(Table)`
     padding-right : 50px !important;
     padding-bottom : 20px !important;
     border: none !important;
-    background: #fff !important;
+    background: ${theme.colors.elementBackground} !important;
 `;
 
 const TableBody = styled(Table.Body)`
-    background: #fff !important;
+    background: ${theme.colors.elementBackground} !important;
     border-top-width: 100px !important;
 `;
 
 const TableRow = styled(Table.Row)`
-    border-color : #fff ;
-    background: #fff ;
+    border-color : ${theme.colors.elementBackground} ;
+    background: ${theme.colors.elementBackground} ;
+    cursor : pointer ;
+    &:hover {
+        background : #ff84a12b !important;
+    };
 `;
 
 const TableHeadcell = styled(Table.HeaderCell)`
@@ -59,6 +70,10 @@ const ButtonEdit = styled(Button)`
 `;
 
 const ButtonAdd = styled(Button)`
+    font-family : 'Kanit', sans-serif !important;
+`;
+
+const HeaderContent = styled(Header)`
     font-family : 'Kanit', sans-serif !important;
 `;
 
@@ -91,38 +106,67 @@ export default enhance( (props)=>
             </Table.Header>
             <TableBody>
                 {
-                    props.list.map( (l)=>
-                        <TableRow key={l.id}>
-                            <TableCell>
-                                <center>{l.id}</center>
-                            </TableCell>
-                            <TableCell>
-                                <Link href="/jobPositions/resume">
-                                    <center>{l.nameJobPositions}</center>
-                                </Link>
-                            </TableCell>
-                            <TableCell>
-                                <center>{l.ReceivingNumber}</center>
-                            </TableCell>
-                            <TableCell>
-                                <center>
-                                    <Link href="/jobPositions/editJobPositions">
-                                        <ButtonEdit animated='fade' size='mini'>
-                                            <Button.Content visible content='แก้ไข'/>
-                                            <Button.Content hidden >
-                                                <Icon name='edit' />
-                                            </Button.Content>
-                                        </ButtonEdit>
-                                    </Link>
-                                    <ButtonAdd animated='fade' size='mini' color="youtube">
-                                        <Button.Content visible content='ลบ'/>
-                                        <Button.Content hidden >
-                                            <Icon name='trash alternate' />
-                                        </Button.Content>
-                                    </ButtonAdd>
-                                </center>
-                            </TableCell>
-                        </TableRow>
+                    props.list.map( (data,i)=> {
+                        return (
+                            <TableRow key={i}>
+                                    <TableCell>
+                                        <Link href="../resume/resume">
+                                            <center>{i+1}</center>
+                                        </Link>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Link href="../resume/resume">
+                                            <center>{data.nameJobPositions}</center>
+                                        </Link>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Link href="../resume/resume">
+                                            <center>{data.ReceivingNumber}</center>
+                                        </Link>
+                                    </TableCell>
+                                
+                                    <TableCell>
+                                    <center>
+                                        <Link href={{ pathname:'/jobPositions/editJobPositions', query: { nameJobPositions : data.nameJobPositions , ReceivingNumber : data.ReceivingNumber }}}>
+                                            <ButtonEdit animated='fade' size='mini'>
+                                                <Button.Content visible content='แก้ไข'/>
+                                                <Button.Content hidden >
+                                                    <Icon name='edit' />
+                                                </Button.Content>
+                                            </ButtonEdit>
+                                        </Link>
+                                        <Modal 
+                                                trigger={
+                                                    <ButtonAdd animated='fade' size='mini' color="youtube" onClick={props.handleModalOpen()}>
+                                                        <Button.Content visible content='ลบ'/>
+                                                        <Button.Content hidden >
+                                                            <Icon name='trash alternate' />
+                                                        </Button.Content>
+                                                    </ButtonAdd>
+                                                }
+                                                size="tiny"
+                                                // onClose={props.handleModalClose()}
+                                                closeIcon
+                                            >
+                                                <HeaderContent icon='archive' content='ลบตำแหน่งงานที่เปิดรับสมัคร' />
+                                                    <Modal.Content>
+                                                        <p>
+                                                            คุณต้องการลบตำแหน่ง <b> {data.nameJobPositions} </b> ใช่หรือไม่ ?
+                                                        </p>
+                                                    </Modal.Content>
+                                                <Modal.Actions>
+                                                    <ButtonAdd onClick={props.handleModalClose()}>
+                                                        <Icon name='remove' /> ยกเลิก
+                                                    </ButtonAdd>
+                                                    <ButtonAdd color='green' onClick={props.handleDeleteJobPositions(i)}>
+                                                        <Icon name='checkmark' /> ตกลง
+                                                    </ButtonAdd>
+                                                </Modal.Actions>
+                                            </Modal>
+                                    </center>
+                                </TableCell>
+                            </TableRow>
+                        )}
                     )
                 }
             </TableBody>
---
+++ pages/resume/addResume.js
@@ -0,0 +1,164 @@
+import React from 'react'
+import { withLayout } from '../../hoc'
+import { compose, withProps , withState , withHandlers } from 'recompose'
+import { TextHeader } from '../../components/TextHeader'
+import styled from 'styled-components'
+import { Form , Button , Icon, Accordion, Divider , Input, Select , options, Label} from 'semantic-ui-react'
+import theme from '../../theme/default'
+import { Breadcrumb2Page } from '../../components/Breadcrumb'
+
+
+const Div = styled.div `
+  position : relative ;
+  background : ${theme.colors.elementBackground};
+  box-shadow : ${theme.colors.boxShadow} ;
+  margin-right : 13px;
+`;
+
+const IconLine = styled(Icon)`
+  font-size: 35px !important;
+`;
+
+const SizeForm = styled(Form)`
+  width: 58% !important;
+  margin-left: 21% !important;
+`;
+
+const BtnSelectFile = styled(Button)`
+  height: 45px !important;
+  margin-top: 22px !important;
+  width: 30% !important;
+  blackground-color: ${theme.colors.ping}
+`;
+
+const DivButton = styled.div`
+  padding-top : 20px !important ;
+  padding-bottom : 60px !important ;
+`;
+
+const ButtonText = styled(Button)`
+  font-family : 'Kanit', sans-serif !important;
+`;
+
+const HR = styled.hr`
+  width: 84.5% !important;
+  margin-top: -2.5% !important;
+  margin-left: 15% !important;
+`;
+
+const SizeInput = styled(Form.Input)`
+  font-size : 16px !important;
+`;
+
+const SizeSelect = styled(Form.Select)`
+  font-size : 16px !important;
+`;
+
+const SizeAccordion = styled(Accordion)`
+  font-size : 16px !important;
+`;
+
+const panels = [
+  {
+    key: 'details',
+    title: 'BlackList',
+    content: {
+      as: Form.TextArea,
+      label: 'รายละเอียด Blacklist :',
+      placeholder: 'กรุณาพิมพ์รายละเอียดการติด Blacklist',
+    },
+  },
+];
+
+const enhance = compose(
+  withProps({
+    pageTitle: 'Add Position'
+  }),
+  withLayout,
+  withState('option','setOption',[{ key: 'w', text: 'รอการสัมภาษณ์', value: 'waiting ' }, { key: 'p', text: 'ผ่านการสัมภาษณ์', value: 'past' } , { key: 'n', text: 'ไม่ผ่านการสัมภาษณ์', value: 'noPast' },  { key: 'b', text: 'Blacklist', value: 'blacklist' },]),
+)
+  
+export default enhance((props) => 
+  <div>
+    {Breadcrumb2Page('ประวัติผู้สมัคร' , 'เพิ่มข้อมูลประวัติผู้สมัคร' , '/resume/resume')}
+    <Divider hidden />
+    <Div>
+      <center>{TextHeader('เพิ่มประวัติผู้สมัคร ( Resume )')}</center>
+      <center>
+        <IconLine name="window minimize outline"/>
+      </center>
+        <SizeForm>
+          <Form.Group widths='equal'>
+            <SizeInput
+              fluid
+              id='name'
+              label='ชื่อ :'
+              placeholder='กรุณาพิมพ์ชื่อ'
+            />
+            <SizeInput
+              fluid
+              id='last-name'
+              label='นามสกุล :'
+              placeholder='กรุณาพิมพ์นามสกุล'
+            />
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <SizeInput
+              maxlength={10}
+              type='number'
+              fluid
+              id='id-card'
+              label='เลขบัตรประจำตัวประชาชน :'
+              placeholder='กรุณาพิมพ์เลขบัตรประจำตัวประชาชน'
+            />
+            <SizeInput
+              fluid
+              id='Rate'
+              label='Rate :'
+              placeholder='กรุณาพิมพ์ Rate'
+            />
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <SizeSelect
+              fluid
+              id='positons'
+              label='ตำแหน่ง :'
+              placeholder='เลือกตำแหน่ง'
+            />
+            <SizeSelect
+              fluid
+              id='status'
+              label='สถานะ :'
+              options={props.option}
+              placeholder='เลือกสถานะ'
+            />
+            
+            
+          </Form.Group>
+          
+          <Form.Group widths='equal'>
+            <SizeInput 
+              id="file" 
+              type="file"
+              label='ไฟล์ประวัติผู้สมัคร :'
+              placeholder='กรุณาเลือกไฟล์ประวัติผู้สมัคร'
+            />
+
+            {/* <label for="file" class="ui icon button" >
+              เลือกไฟล์
+            </label> */}
+            
+          </Form.Group>
+          <SizeAccordion  panels={panels} /> <HR/>
+          <DivButton>
+              <ButtonText floated='right' positive>
+                <Icon name='checkmark' /> บันทึก
+              </ButtonText>
+              <ButtonText floated='right'>
+                <Icon name='times' /> ยกเลิก
+              </ButtonText>
+          </DivButton>
+        </SizeForm>
+    </Div>
+  </div>
+);
